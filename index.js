a6469dbb2655246b5a30355ad352ed6fb854f523
fabrice404 <fabrice.lamant@gmail.com>
2018-01-03 20:35:15 +0100

    Add args parameters

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+const minimist = require('minimist')
 const customer = require('./customer')
 const party = require('./party')
 
-const latitude = 53.339428
-const longitude = -6.257664
-const maxDistance = 100
-const file = 'data/customers.json'
+const args = minimist(process.argv)
+const latitude = args.lat || 53.339428
+const longitude = args.lng || -6.257664
+const maxDistance = args.distance || 100
+const file = args.file || 'data/customers.json'
 
 customer.loadCustomers(file)
   .then(customers => {
     let invited = party.listInvitedCustomers(latitude, longitude, maxDistance, customers)
     for (let i = 0; i < invited.length; i++) {
-      var customer = invited[i]
-      console.log(customer.name)
+      console.log(invited[i].name)
     }
   })
   .catch(error => console.error(error))
